1c53c1fec899f6ee98ba7a2e82808022bc6221aa
Master-mohit <mohitmansigupta9098@gmail.com>
2024-12-18 13:05:13 +0530

    context

--- game/src/components/Contact.jsx
+++ game/src/components/Contact.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { mycontext } from '../contextStore/Context';
 
 const Contact = () => {
+   const {plus, setplus} = useContext(mycontext)
+  const plusHandler = () => {
+    setplus(plus+1);
+   }
   return (
-    <div className='text-[2vw] flex items-center justify-center h-screen w-screen bg-black text-white'>
-      this is contact page
+    <div className=' flex-col text-[2vw] flex items-center justify-center h-screen w-screen bg-black text-white'>
+      <div className='text-[3vw] border p-10 text-white bg-zinc-600 font-black '>
+       {plus}
+      </div>
+      <div onClick={plusHandler} className='border rounded p-4 mt-10 text-[30px] text-white '>
+        plus❕
+      </div>
     </div>
   )
 }
